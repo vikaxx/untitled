a4d658827c21c4857ae29d630908d4cd04439471
vikaxx <victoria.aliaeva@gmail.com>
2020-12-08 23:52:07 +0200

    more helloes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
